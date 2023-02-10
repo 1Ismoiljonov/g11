2df479c2eaf7d3c3f0c592dc271f148e84156a0a
1Ismoiljonov <samandarismoiljonov9@gmail.com>
2023-02-10 10:06:32 +0500

    test

--- node.js
+++ node.js
@@ -1,1 +1,18 @@
-console.log('Samandar');
+// number
+
+// integer => butun sonlar
+// float => kasir sonlar 
+ 
+// let num = 1000000 
+// let num2 = 1000_000
+
+// console.log(num === num2);
+
+// let a = '1'
+// let b = 2 
+// console.log(parseInt(a)+b);
+// console.log(+a+b);
+
+let a = 10
+let b = 1e1
+console.log(a + b);
