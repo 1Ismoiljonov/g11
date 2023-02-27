b07451fe3b927170dbf1fe0de7871a53fb943825
1Ismoiljonov <samandarismoiljonov9@gmail.com>
2023-02-27 09:09:35 +0500

    testing

--- node.js
+++ node.js
@@ -1,1 +1,23 @@
-console.log('Samandar')
+// let a = 26
+// let b = 20 
+// if (a>b){
+//     console.log(a-b);
+// } else {
+//     console.log(b-a);
+// }
+
+// function sum (a, b) {
+//     if (a>b) {
+//         console.log(a-b);
+//     } else{
+//         console.log(b-a);
+//     }
+
+// }    
+
+// sum(23, 29)
+
+// let name = `Ismoiljonov Samandar`
+// console.log(name.indexOf('dar'));
+// console.log(name.lastIndexOf('dar'));
+
