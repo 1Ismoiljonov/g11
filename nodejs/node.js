1a650978c9c12ae9b86f50a22fc31815d31f2b54
1Ismoiljonov <samandarismoiljonov9@gmail.com>
2023-03-01 11:38:22 +0500

    test

--- nodejs/node.js
+++ nodejs/node.js
@@ -25,9 +25,27 @@
 // console.log(num.startsWith('+998'));
 // console.log('samandar');
 
-let names = 'Ismoiljonov Samandar'
-for (i of names) {
-    console.log(i === i.toUpperCase());
-}
+// let names = 'Ismoiljonov Samandar'
+// for (i of names) {
+//     console.log(i === i.toUpperCase());
+// }
 
-console.log('Samandar');
+// console.log('Samandar');
+// let ar = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// let [bir, ikki, uch, tort, ...rest] = ar
+// console.log(rest);
+
+// let date = new Date()
+
+// console.log(date.getDate());   // shu kungi sana  
+// console.log(date.getDay());   // hafta kuni
+// console.log(date.getHours());    // hozirgi soatni
+// console.log(date.getMinutes());    // hozirgi minutni
+// console.log(date.getSeconds());    // hozirgi second
+// console.log(date.getTime());    // bu 1970 => yildan beri necha sekond o'tganini hisonblaydi
+
+// console.log(date.get);
+
+let obj = {login: 'Samandar', pw: 9302003}
+
+localStorage.setItem('info', obj)
